6e8bde68ef8d456b287b948634b95c694dc09c1b
Steven Zeil <zeil@cs.odu.edu>
2022-06-27 13:46:22 -0400

    add module-info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module edu.odu.cs.zeil.codegrader {
+    requires com.fasterxml.jackson.dataformat.yaml;
+    requires com.fasterxml.jackson.databind;
+    requires org.slf4j;
+    requires java.logging;
+
+    exports edu.odu.cs.zeil.codegrader;
+}
