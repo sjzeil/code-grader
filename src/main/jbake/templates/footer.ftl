7084e5dae059eafb958aac0ade9ea998a1a53d57
Steven Zeil <zeil@cs.odu.edu>
2022-07-13 12:20:21 -0400

    set up gh-pages branch

--- src/main/jbake/templates/footer.ftl
+++
@@ -1,8 +0,0 @@
-    </div>
-    
-    <div class="footer">
-    &copy; 2020 Old Dominion University
-    </div>
-        
-  </body>
-</html>
--- test002.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-mkdir build
-mkdir build/test001
-cp -rf src/test/assignment build/test001/
-bash -x src/prototypes/bash/testSubmission.sh build/test001/assignment/tests  build/test001/assignment/submissions/bad  build/test001/assignment/gold
